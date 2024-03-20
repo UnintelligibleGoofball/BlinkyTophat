bc53159ef01d243a13054560177459da5adeb8aa
Blue <108904799+UnintelligibleGoofball@users.noreply.github.com>
2024-03-19 22:25:30 -0500

    Better comments

--- Test.py
+++ Test.py
@@ -1,7 +1,10 @@
+#neopixel library thats kinda jank but works better than the normal one
 from neopixel import Neopixel
 
+#defines neopixel object
+#Neopixel(number of LED's, state machine ID, GPIO number, mode(RGB or RGBW))
 pixels = Neopixel(10, 0, 0, "RGB")
 
-pixels.fill((100,0,150))
+pixels.fill((100,0,150)) #every defined pixel set to a light blue
 
-pixels.show()
+pixels.show() #shows the pixels
