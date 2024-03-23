43259180621cd49e7a5b3ab9bfc344fb73fd74c6
Blue <108904799+UnintelligibleGoofball@users.noreply.github.com>
2024-03-23 16:20:21 -0500

    progress, people, progress

--- Pico/Test.py
+++ Pico/Test.py
@@ -1,10 +1,16 @@
-#neopixel library thats kinda jank but works better than the normal one
 from neopixel import Neopixel
+import numpy as np
+
+PixelNum = 296 #number of pixels
+PixelWidth = 37 #how many pixels wide
+PixelHight = 8 #how many pixels tall
+
+PixelArr = np.arange(PixelNum).reshape(PixelHight, PixelWidth) #creates an array of pixels with the proper dimensions
 
 #defines neopixel object
-#Neopixel(number of LED's, state machine ID, GPIO number, mode(RGB or RGBW))
-pixels = Neopixel(10, 0, 0, "RGB")
-
-pixels.fill((100,0,150)) #every defined pixel set to a light blue
+pixels = Neopixel(PixelNum, 0, 0, "RGB") #Neopixel(number of LED's, state machine ID, GPIO number, mode(RGB or RGBW))
 
 pixels.show() #shows the pixels
+
+def ShowArt(art):
+    for i in range(PixelNum):
