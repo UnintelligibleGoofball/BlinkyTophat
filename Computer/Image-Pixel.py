1bb6664f23d7b9771cfbad9c79949670ad79524a
Blue <108904799+UnintelligibleGoofball@users.noreply.github.com>
2024-03-23 15:49:01 -0500

    Converts image to text file locally, then puts text file with pico!

--- Computer/Image-Pixel.py
+++ Computer/Image-Pixel.py
@@ -1,5 +1,15 @@
 import numpy as np
 from PIL import Image
-img = Image.open('lena.png')
-arr = np.array(img) # 640x480x4 array
-arr[20, 30] # 4-vector, just like above
+img = Image.open('./Computer/HAT_ART.png')
+arr = np.array(img) # 37x8 array
+
+# Program to save a NumPy array to a text file
+
+# Displaying the array
+print('Array:\n', arr)
+file = open("./Pico/Art/HAT_ART_ARRAY.txt", "w+")
+
+# Saving the array in a text file
+content = str(arr)
+file.write(content)
+file.close()
