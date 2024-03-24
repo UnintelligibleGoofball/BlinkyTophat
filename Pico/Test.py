513a44b5a8319ee1a766ae891c44b2ccc0328989
Blue <108904799+UnintelligibleGoofball@users.noreply.github.com>
2024-03-23 22:30:02 -0500

    Added a fun swirly test pattern

--- Pico/Test.py
+++ Pico/Test.py
@@ -1,5 +1,7 @@
 from neopixel import Neopixel
 from ulab import numpy as np
+import machine, time
+import rp2
 
 ProgExec = 1
 
@@ -23,5 +25,17 @@ def ShowArt(art):
             pixels.set_pixel(PixelArr[i][j], (artArr[i][j][0], artArr[i][j][1], artArr[i][j][2]))
     pixels.show()
 
+def TestSwirl():
+    for i in range(0, PixelNum):
+        pixels.set_pixel(i, (85, 85, 85))
+        pixels.set_pixel(i-8, (0, 0, 0))
+        time.sleep(.1)
+        pixels.show()
+
+#FIRST logo and team number
 if ProgExec == 1:
     ShowArt('HAT_ART')
+
+#run through 8 at a time white (to see if larger strips are calibrated without having to power  all at once)
+if ProgExec == 2:
+    TestSwirl()
