5cd8f271af0f3183b27484c3c692592c8a8269df
Blue <108904799+UnintelligibleGoofball@users.noreply.github.com>
2024-03-23 19:07:27 -0500

    broke ;(

--- Computer/Image-Pixel.py
+++ Computer/Image-Pixel.py
@@ -1,15 +1,5 @@
 import numpy as np
 from PIL import Image
 img = Image.open('./Computer/HAT_ART.png')
-arr = np.array(img) # 37x8 array
+arr = np.save('./Pico/Art/HAT_ART_ARRAY', img) # 37x8 array
 
-# Program to save a NumPy array to a text file
-
-# Displaying the array
-print('Array:\n', arr)
-file = open("./Pico/Art/HAT_ART_ARRAY.txt", "w+")
-
-# Saving the array in a text file
-content = str(arr)
-file.write(content)
-file.close()
--- Pico/Test.py
+++ Pico/Test.py
@@ -1,16 +1,24 @@
 from neopixel import Neopixel
-import numpy as np
+from ulab import numpy as np
+
+ProgExec = 1
 
 PixelNum = 296 #number of pixels
 PixelWidth = 37 #how many pixels wide
 PixelHight = 8 #how many pixels tall
 
-PixelArr = np.arange(PixelNum).reshape(PixelHight, PixelWidth) #creates an array of pixels with the proper dimensions
+PixelArr = np.arange(PixelNum).reshape((PixelHight, PixelWidth)) #creates an array of pixels with the proper dimensions
 
 #defines neopixel object
 pixels = Neopixel(PixelNum, 0, 0, "RGB") #Neopixel(number of LED's, state machine ID, GPIO number, mode(RGB or RGBW))
 
-pixels.show() #shows the pixels
-
 def ShowArt(art):
+    artFileName = art + '_ARRAY.npy'
+    artFile = open(artFileName, 'r')
+    artArr = np.load(artFileName)
     for i in range(PixelNum):
+        pixels.set_pixel(PixelArr[i], artArr[i])
+    pixels.show()
+
+if ProgExec == 1:
+    ShowArt('HAT_ART')
